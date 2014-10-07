1699bdbf5855d538e7d5ae9db5f0488672030a9a
Winston Kim <winzton.kim@gmail.com>
2014-10-07 03:28:55 -0700

    toolbar waypoints

---
+++ js/custom.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+	$('#hours_locations').waypoint(function(direction) {
+		if (direction == 'down'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#hours_nav').addClass('active');
+			$('#navbar').removeClass('nav_transparent');
+		}
+	})
+
+	$('#hours_locations').waypoint(function(direction) {
+		if (direction == 'up'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#home_nav').addClass('active');
+			$('#navbar').addClass('nav_transparent');
+		}
+	})
+
+	$('#services').waypoint(function(direction) {
+		if (direction == 'down'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#services_nav').addClass('active');
+		}
+	})
+
+	$('#services').waypoint(function(direction) {
+		if (direction == 'up'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#hours_nav').addClass('active');
+		}
+	})
+
+	$('#jobs').waypoint(function(direction) {
+		if (direction == 'down'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#jobs_nav').addClass('active');
+		}
+	})
+
+	$('#jobs').waypoint(function(direction) {
+		if (direction == 'up'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#services_nav').addClass('active');
+		}
+	})
+
+	$('#contactus').waypoint(function(direction) {
+		if (direction == 'down'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#contact_nav').addClass('active');
+		}
+	})
+
+	$('#contactus').waypoint(function(direction) {
+		if (direction == 'up'){
+			$('a.smoothScroll').parent().removeClass('active');
+			$('#jobs_nav').addClass('active');
+		}
+	})
+
+});
